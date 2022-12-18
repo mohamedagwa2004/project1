92621ea4a395da31de9ea75a43e0876008777f91
mohamedagwa2004 <120852466+mohamedagwa2004@users.noreply.github.com>
2022-12-18 03:17:41 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics.CodeAnalysis;
+
+int num, x, sum, Starting, Ending;
+
+Console.WriteLine("Please Enter The Primer Number");
+
+Console.WriteLine("Start");
+Starting = int.Parse(Console.ReadLine());
+
+Console.WriteLine("End");
+Ending = int.Parse(Console.ReadLine());
+
+for (num = Starting; num<= Ending; num++)
+{
+    sum = 0;
+
+    for (x = 2; x <= num / 2 ; x++ )
+    {
+       if (num % x ==0)
+            {
+                sum++;
+                break;
+            }
+    }
+    if (sum == 0 && num != 1)
+        Console.WriteLine("{0}", num);
+}
+Console.WriteLine("Ending Process");
+Console.ReadKey();
